acecce9c246f6f806b594cdf8eb9f3291e5b8734
Jakob Kinne <8141@htl.rennweg.at>
2024-12-25 03:01:39 +0100

    Added sql select parsing

--- src/V2/scripts/SimpleSql/SimpleSqlCore.js
+++ src/V2/scripts/SimpleSql/SimpleSqlCore.js
@@ -237,44 +237,269 @@ class SQLStatement
 
     static parse(statement)
     {
-        statement = statement.toLower();
+        console.log(statement);
+        
+        statement = SQLStatement._splitStatement(statement.toLowerCase());
+
+        console.log(statement);
 
         let keyWords = {
-            "select": this.parseSelect,
-            "insert into": this.parseInsert,
-            "update": this.parseUpdate,
-            "delete": this.parseDelete
+            "select": this._parseSelect,
+            "insert,into": this._parseInsert,
+            "update": this._parseUpdate,
+            "delete,from": this._parseDelete
         };
 
-        let statementType = "";
+        let statementType;
         
         for (let keyWord of Object.keys(keyWords))
         {
-            if (statement.startWith(keyWord))
+            if (keyWord == statement.slice(0, keyWord.split(",").length))
             {
-                statementType = keyWord;
+                statementType = keyWords[keyWord];
                 break;
             }
         }
 
-        if (statementType == "")
+        if (statementType === undefined)
         {
-            throw "Error while parsing sql statement!";
+            throw `${statement[0]} isn't valid statement`;
         }
 
         return (statementType)(statement);
     }
 
+    static _splitStatement(statement)
+    {
+        let specialSplitters = [",", "<", ">", "=", "<=", ">=", "(", ")", ";"];
+        let splittedStatement = [];
+
+        let phrase = "";
+
+        let inString = false;
+
+        while (statement != "")
+        {
+            let char = statement.charAt(0);
+            statement = statement.slice(1);
+
+            if (char == "'")
+            {
+                inString = !inString;
+            }
+
+            if (char == " " && !inString)
+            {
+                if (phrase != "") {
+                    splittedStatement.push(phrase);
+                    phrase = "";
+                }
+                continue;
+            }
+            else if (specialSplitters.includes(char))
+            {
+                if (phrase != "")
+                {
+                    splittedStatement.push(phrase);
+                }
+
+                if (statement.length > 1)
+                {
+                    if (specialSplitters.includes(char + statement.charAt(0)))
+                    {
+                        char += statement.charAt(0);
+                        statement = statement.slice(1);
+                    }
+                }
+
+                splittedStatement.push(char);
+                phrase = "";
+                continue;
+            }
+
+            phrase += char;
+        }
+
+        if (phrase != "")
+        {
+            splittedStatement.push(phrase);
+        }
+
+        return splittedStatement;
+    }
+
+    static _parseWhereClause(statement)
+    {
+        if (statement[0] != "where")
+        {
+            throw `Expected where but found ${statement[0]}`;
+        }
+
+        statement = statement.splice(1);
+
+        let conjunctors = ["or", "and"];
+        let operators = ["=", "<", ">", ">=", "<=", "is"];
+
+        let columns = [];
+        let condition = "";
+
+        let notFlag = 0;
+        let dataFlag = true;
+        let operatorFlag = false;
+        let conjuctorFlag = false;
+
+        while (statement.length > 0)
+        {
+            if (dataFlag && !operatorFlag && statement[0] == "not")
+            {
+                notFlag++;
+                condition += "!(";
+            }
+            else if (dataFlag)
+            {
+                if (conjunctors.includes(statement[0]) || operators.includes(statement[0]))
+                {
+                    throw `Expected data but found ${statement[0]}`;
+                }
+
+                condition += statement[0];
+
+                if (statement[0] != "(")
+                {
+                    dataFlag = false;
+
+                    if (operatorFlag)
+                    {
+                        operatorFlag = false;
+                        conjuctorFlag = true;
+                    }
+                    else
+                    {
+                        operatorFlag = true;
+                        if (!columns.includes(statement[0]))
+                        {
+                            columns.push(statement[0]);
+                        }
+                    }
+                }
+            }
+            else if (operatorFlag)
+            {
+                if (!operators.includes(statement[0]))
+                {
+                    throw `Expected operator but found ${statement[0]}`;
+                }
+
+                condition += statement[0].replace(/(?<![<>])=/g, "==").replace("is", "===");
+
+                dataFlag = true;
+            }
+            else if (conjuctorFlag)
+            {
+                if (!conjunctors.includes(statement[0]) && statement[0] != ")")
+                {
+                    throw `Expected conjunctor but found ${statement[0]}`;
+                }
+
+                if (notFlag > 0)
+                {
+                    condition += ")";
+                    notFlag--;
+                }
+
+                condition += statement[0].replace("and", "&&").replace("or", "||");
+
+                if (statement[0] != ")" || statement.length == 1)
+                {
+                    conjuctorFlag = false;
+                    dataFlag = true;
+                }
+            }
+
+            statement = statement.splice(1);
+        }
+
+        return {
+            "columns": columns,
+            "condition": new Function(`return (${columns}) => ${condition}`)()
+        };
+    }
+
     static _parseSelect(statement)
     {
-        statement = statement.slice(7); // "select ".length
+        statement = statement.splice(1);
+        console.log(statement);
+        
+        let columns = [];
+        let table = "";
+        let conditions = [];
 
-        let columnsToSelect = [];
+        let dataFlag = true;
 
-        if (!statement.startWith("*"))
+        if (statement[0] != "*")
         {
-            
+            while (statement[0] != "from")
+            {
+                if (dataFlag && statement[0] == ",")
+                {
+                    throw `Expected column name but found ,`;
+                }
+                else if (!dataFlag && statement[0] != ",")
+                {
+                    throw `Expected , but found ${statement[0]}`;
+                }
+
+                if (dataFlag)
+                {
+                    if (statement[0].includes(" "))
+                    {
+                        throw `Expected column name but found ${statement[0]}`;
+                    }
+                    columns.push(statement[0]);
+                }
+
+                dataFlag = !dataFlag;
+                statement = statement.splice(1);
+            }
         }
+        else
+        {
+            statement = statement.splice(1);
+        }
+
+        console.log(columns);
+        console.log(statement);
+
+        if (statement[0] != "from")
+        {
+            throw `Expected from but found ${statement[0]}`;
+        }
+
+        statement = statement.splice(1);
+        
+        if (statement[0] == "where")
+        {
+            throw `Expected table name but found where`;
+        }
+
+        table = statement[0];
+
+        if (table.includes(" "))
+        {
+            throw `Expected table name but found ${table}`;
+        }
+
+        statement = statement.splice(1);
+
+        console.log(table);
+        console.log(statement);
+
+        if (statement.length != 0)
+        {
+            conditions = SQLStatement._parseWhereClause(statement);
+        }
+
+        return [table, columns, conditions];
     }
 
     static _parseInsert()
